463d4d8ad03d746bb74530ee27452413930a79c5
mrioshe <80715697+diperezp@users.noreply.github.com>
2023-11-21 16:30:54 -0500

    Codificacion del ejercicio 10

--- Tareas/Actividad_2/EjercicioResuelto10/src/ejercicioresuelto10/EjercicioResuelto10.java
+++ Tareas/Actividad_2/EjercicioResuelto10/src/ejercicioresuelto10/EjercicioResuelto10.java
@@ -13,8 +13,41 @@ public class EjercicioResuelto10 {
     
     public static void main(String[] args) {
         
+        //Definimos un objeto de la class Estudiante
+        Estudiante estudiante1= new Estudiante();
+        
+        //Definimos un objeto de la class Scanner
+        Scanner inPut=new Scanner(System.in);
+        
         //Numero de inscripcion
-        int ni=
+        System.out.print("Inserte el numero de inscripcion: ");
+        estudiante1.ni=inPut.nextInt();
+        
+        //Nombre del estudiante
+        System.out.print("Introduce el nombre del estudiante: ");
+        estudiante1.nom=inPut.next();
+        
+        //Patrimonio del estudiante 
+        System.out.print("Introduce el patrimonio del estudiante: ");
+        estudiante1.pat=inPut.nextDouble();
+        
+        //Estrato del estudiante
+        System.out.print("Introduce el estrato del estudiante: ");
+        estudiante1.est=inPut.nextInt();
+        
+        //Espacio entre informacion de entrada y salida
+        System.out.println("");
+        
+        //imprimimos la informacion del estudiante
+        System.out.println("Numero de inscripcion (NI): "+estudiante1.ni);
+        
+        System.out.println("Nombre (NOM): "+estudiante1.nom);
+        
+        System.out.println("Patrimonio (PAT): "+estudiante1.pat);
+        
+        System.out.println("Estrato (ES): "+estudiante1.est);
+        
+        System.out.println("Valor de Pago de Matricula: "+estudiante1.calcMat());
     }
     
 }
