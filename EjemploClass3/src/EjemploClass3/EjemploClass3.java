9a8da43ed909f0c76e07ee4e5731473e66a31df0
diperezp <diperezp@unal.edu.co>
2024-02-26 20:22:28 -0500

    Creacion de proyecto para estudiar el concepto de class

---
+++ EjemploClass3/src/EjemploClass3/EjemploClass3.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * @author diego
+ */
+package EjemploClass3;
+
+
+public class EjemploClass3 {
+
+    public static void main(String[] args) {
+        Estudiante estudiante1 = new Estudiante();
+        
+        estudiante1.ID=1002063852;
+        estudiante1.name="Diego";
+        estudiante1.lastname="Pineda";
+        estudiante1.Phone=31586;
+        
+        System.out.println("Identificacion: "+estudiante1.ID);
+        System.out.println("Nombre: "+estudiante1.name);
+        System.out.println("Apellido: "+estudiante1.lastname);
+        System.out.println("Numero de Telefono: "+estudiante1.Phone);
+
+
+
+                
+    }
+    
+}
---
+++ EjemploClass3/src/EjemploClass3/Estudiante.java
@@ -0,0 +1,15 @@
+
+package EjemploClass3;
+
+/**
+ *
+ * @author diego
+ */
+public class Estudiante {
+    //Datos del estudiante
+    int ID;
+    String name;
+    String lastname;
+    String Address;
+    int Phone;
+}
