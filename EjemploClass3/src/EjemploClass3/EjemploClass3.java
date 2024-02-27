e5cb25f89ce9832d66222cd96a8538c49abfd460
diperezp <diperezp@unal.edu.co>
2024-02-26 20:52:49 -0500

    Clase4 y Clase3

--- EjemploClass3/src/EjemploClass3/EjemploClass3.java
+++ EjemploClass3/src/EjemploClass3/EjemploClass3.java
@@ -19,6 +19,18 @@ public class EjemploClass3 {
         System.out.println("Nombre: "+estudiante1.name);
         System.out.println("Apellido: "+estudiante1.lastname);
         System.out.println("Numero de Telefono: "+estudiante1.Phone);
+        
+        
+        Empleado empleado1 = new Empleado();
+        
+        empleado1.ID=15518;
+        empleado1.name="Jairo";
+        empleado1.lastname="Lopez";
+        empleado1.Address="Calle 50 # 50-139";
+        empleado1.Phone=31589;
+        empleado1.Empresa="unal";
+        empleado1.Salario=400000;
+        
 
 
 
--- EjemploClass3/src/EjemploClass3/Empleado.java
+++ EjemploClass3/src/EjemploClass3/Empleado.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package EjemploClass3;
 
 /**
  *
  * @author diego
  */
-public class Empleado {
-    
+public class Empleado extends Estudiante{
+    String Empresa;
+    double Salario;
 }
