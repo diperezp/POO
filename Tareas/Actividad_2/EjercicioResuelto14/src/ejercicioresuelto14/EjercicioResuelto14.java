968e7637287d709cd1def9ade5c2739ce56fbde9
mrioshe <80715697+diperezp@users.noreply.github.com>
2024-03-06 09:51:21 -0500

    Creacion de la clase del ejercicio 14 de la actividad 2

--- Tareas/Actividad_2/EjercicioResuelto14/src/ejercicioresuelto14/EjercicioResuelto14.java
+++ Tareas/Actividad_2/EjercicioResuelto14/src/ejercicioresuelto14/EjercicioResuelto14.java
@@ -1,18 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package ejercicioresuelto14;
-
 /**
  *
  * @author diego
+ * @date 06-03-2024
+ * @brief Solucion del ejercicio resuelto 14 del capitulo 4, del libro de 
+ * logica de programacion de Efrain Oviedo
  */
+package ejercicioresuelto14;
+
+
 public class EjercicioResuelto14 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
---
+++ Tareas/Actividad_2/EjercicioResuelto14/src/ejercicioresuelto14/VentasPagosEmpresa.java
@@ -0,0 +1,72 @@
+/**
+ * @author diego
+ * @date 06-05-2024
+ * @brief Ventas y pagos de una empresa
+ */
+package ejercicioresuelto14;
+
+
+public class VentasPagosEmpresa {
+
+    
+    /*Definimos los atributos de la clase*/
+    
+    //Definimos las ventas que se hacen por departamentos
+    double ventasDepart1;
+    double ventasDepart2;
+    double ventasDepart3;
+    
+    //Salario Para cada departamento
+    double salaryDepart;
+    
+    //Definimos los salarios por departamentos
+    double salaryDepart1;
+    double salaryDepart2;
+    double salaryDepart3;
+    
+    //Ventas totales de la empresa
+    public double ventasTotal;
+    
+    //33% de las ventas
+    public double valuePercentIncetiv;
+
+    
+    /*Definimos los metodos de la clase*/
+    
+    //Calculo de los porcentajes y del salario de los departamentos
+    public void calcPagosEmpresa(){
+        
+        //Calculo del valor de ventas totales de la empresa
+        ventasTotal=salaryDepart1+salaryDepart2+salaryDepart3;
+        
+        //El valor correspondiente al 33% de las ventas
+        valuePercentIncetiv=ventasTotal*0.33;
+        
+        //Calculo del salario de los departamentos
+        
+        //Evaluamos si el departamento 1 aplica para el incentivo
+        if(ventasDepart1>valuePercentIncetiv){
+            salaryDepart1=salaryDepart*1.2;  //mas 20% de incentivo
+        }
+        
+        //Evaluamos si el departamento 2 aplica para el incentivo
+        if (ventasDepart2>valuePercentIncetiv){
+            salaryDepart2=salaryDepart*1.2;  // mas 20% de incentivo
+        }
+        
+        //Evaluamos si el depatamento 3 aplica para el incentivo
+        
+        if(ventasDepart3>valuePercentIncetiv){
+            salaryDepart3=salaryDepart*1.2; //mas 20% de incentivo
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
