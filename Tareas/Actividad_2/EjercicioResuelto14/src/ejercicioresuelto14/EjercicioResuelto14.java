1596387bd4a128c62c3cd8b47ae6369e721f1baf
diperezp <diperezp@unal.edu.co>
2024-03-06 21:22:32 -0500

    Update EjercicioResuelto14.java

--- Tareas/Actividad_2/EjercicioResuelto14/src/ejercicioresuelto14/EjercicioResuelto14.java
+++ Tareas/Actividad_2/EjercicioResuelto14/src/ejercicioresuelto14/EjercicioResuelto14.java
@@ -7,11 +7,44 @@
  */
 package ejercicioresuelto14;
 
+//Importamos la class Scanner
+import java.util.Scanner;
+
 
 public class EjercicioResuelto14 {
 
 
     public static void main(String[] args) {
+        //Instanciamos la Class Scanner
+        Scanner inPut= new Scanner(System.in);
+        
+        //Instanciamos un objeto de la clase VentasPagosEmpresa
+        VentasPagosEmpresa pagos= new VentasPagosEmpresa();
+        
+        //Pedimos por consola el valor de ventas del departamento 1
+        System.out.print("Introduce las ventas del departamento 1: ");
+        pagos.ventasDepart1=inPut.nextDouble();
+        
+        //Pedimos por consola el valor de ventas del departamento 2
+        System.out.print("Introduce las ventas del departamento 2: ");
+        pagos.ventasDepart2=inPut.nextDouble();
+        
+        //Pedimos por consola el valor de ventas del departamento 3
+        System.out.print("Introduce las ventas del departamento 3: ");
+        pagos.ventasDepart3=inPut.nextDouble();
+        
+        //Pedimos por consola el salario de los departamentos
+        System.out.println("Introduce el salario del departamento i: ");
+        pagos.salaryDepart=inPut.nextDouble();
+        
+        //Hacemos el calculo de los salarios y los incentivos de los salarios
+        pagos.calcPagosEmpresa();
+        
+        //Imprimimos la informacion de los pagos de los empleados
+        
+        
+        
+        
 
     }
     
