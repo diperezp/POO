5463b9e925fb3b8f20b992f0fed803b0b029fc18
mrioshe <80715697+diperezp@users.noreply.github.com>
2024-07-23 12:02:47 -0500

    feedback de  como crear interfaz graficas

---
+++ Semanas/Semana7/FeedBackInterfaz/src/feedbackinterfaz/FeedBackInterfaz.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package feedbackinterfaz;
+
+/**
+ *
+ * @author if401-09
+ */
+public class FeedBackInterfaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pantalla window = new Pantalla();
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+        
+    }
+    
+}
