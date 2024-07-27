6d1bccb268c5dd891ba9a6e8e21ee97e2d6088b4
diperezp <diperezp@unal.edu.co>
2024-07-27 07:41:02 -0500

    funcionalidad de los botones

---
+++ Tareas/Actividad_3/feedbackinterfaz/src/feedbackinterfaz/Feedbackinterfaz.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package feedbackinterfaz;
+
+/**
+ *
+ * @author diego
+ */
+public class Feedbackinterfaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        Pantalla window = new Pantalla();
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+    }
+    
+}
