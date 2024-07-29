c91491b7e7854de921edfb967ee3e6c587c7a402
mrioshe <80715697+diperezp@users.noreply.github.com>
2024-07-29 15:44:53 -0500

    Update Estudiante.java

--- Semanas/Semana7/EjercicioResuelto10/src/ejercicioresuelto10/Estudiante.java
+++ Semanas/Semana7/EjercicioResuelto10/src/ejercicioresuelto10/Estudiante.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ *
+ * @author diego
+ * @date 03-03-2024
+ * @brief Datos socioeconomicos de un estudiante
  */
 package ejercicioresuelto10;
 
-/**
- *
- * @author if401-09
- */
 public class Estudiante {
+    //Definimos los atributos socioeconomicos del Estudiante;
+    //Numero de inscripcion
+    int ni;
+    //Nombre 
+    String nom;
+    //Patrimonio
+    double pat;
+    //Estrato social
+    int est;
+    //Valor de matricula
+    private double pagMat;
+    
+    //Definimos los metodos de la class
+    //Metodo del calculo de la matricula
+    public double calcMat(){
+        
+        //Valor minimo de matricula
+        pagMat=50000;
+        
+        //Excedente dependiente del estado socioeconomico
+        if(pat>2000000 & est>3){
+            pagMat+=pat*0.03; //mas 3% del patrimonio
+        }
+        return pagMat;
+    }
+    
     
 }
