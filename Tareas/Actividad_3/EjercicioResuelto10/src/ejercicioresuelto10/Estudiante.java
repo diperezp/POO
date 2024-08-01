6a1d0e453879d3041586ed915bbe468b2ff2e88f
diperezp <diperezp@unal.edu.co>
2024-07-31 19:42:14 -0500

    Inicio de la Tercera Tarea de la actividad 3

---
+++ Tareas/Actividad_3/EjercicioResuelto10/src/ejercicioresuelto10/Estudiante.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicioresuelto10;
+
+/**
+ *
+ * @author diego
+ */
+public class Estudiante {
+    
+}
